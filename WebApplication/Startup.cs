bc929294b6718afaf2d329079157d2b43937cd99
usman_solemn <usman.solemn@gmail.com>
2021-04-04 23:49:10 +0500

    added hangfire

---
+++ WebApplication/Startup.cs
@@ -0,0 +1,66 @@
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication", Version = "v1" });
+            });
+
+            services.AddHangfire(h => h.UseInMemoryStorage());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IBackgroundJobClient backgroundJobClient)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication v1"));
+            }
+
+            app.UseHangfireServer();
+
+            //Basic Authentication added to access the Hangfire Dashboard  
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions()
+            {
+                DashboardTitle = "Jobs Dashboard"
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            backgroundJobClient.Enqueue(() => Console.WriteLine("Fire-and-forget!"));
+        }
+    }
+}
