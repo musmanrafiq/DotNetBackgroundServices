8df3da6548913e6d59b07047e63952ede50dceb1
usman_solemn <usman.solemn@gmail.com>
2021-04-04 23:59:20 +0500

    added background queue and concurrent queue

---
+++ WebApplication/BackgroundServices/QueueService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication.BackgroundServices
+{
+    public class QueueService : BackgroundService
+    {
+        private IBackgroundQueue _queue;
+
+        public QueueService(IBackgroundQueue queue)
+        {
+            _queue = queue;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var task = await _queue.PopQueue(stoppingToken);
+
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                using (var source = new CancellationTokenSource())
+                {
+                    source.CancelAfter(TimeSpan.FromMinutes(1));
+                    var timeoutToken = source.Token;
+
+                    await task(timeoutToken);
+                }
+            }
+        }
+    }
+}
